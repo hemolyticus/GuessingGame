1a9b851bd2699a77516de8a6a296e38916d5a0d9
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-06-28 10:30:34 +1200

    Created a simple condition statement

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 /**
  * Created by meljohndelbarrio on 28/06/17.
  */
+//Create a secret number
+
+var secretNumber = 4;
+//Ask user for guess
+
+var guess = prompt("Guess a number");
+
+//Check if guess is correct
+
+if (Number(guess) === secretNumber)
+{
+    alert("You guessed the number right");
+}
+else if (Number(guess) > secretNumber)
+{
+ alert("Too high");
+
+}else
+{
+    alert("Too low");
+}
